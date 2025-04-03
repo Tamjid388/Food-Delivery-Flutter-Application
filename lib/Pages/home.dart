42dfb66a196ec59a65c18e796b134b6bb5500b38
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-04-03 17:32:02 +0600

    Created AppWidget with reusable text styles

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:food_delivery_application/Widgets/widget_support.dart";
 
 class Home extends StatefulWidget {
   const Home({super.key});
@@ -12,14 +13,38 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body:Container(
-        margin: EdgeInsets.only(top: 50.0,left:10.0,right:10.0),
-        child:Column(children: [
-        Text("Hello,Tamjid Ahmed",
-        style: TextStyle(
-          color: Colors.black,fontSize: 20.0,
-          fontWeight:FontWeight.bold,fontFamily: "Poppins"
-        ),)
-      ],),),
+        margin: EdgeInsets.only(top: 30.0,left:10.0,right:10.0),
+        child:Column(
+          crossAxisAlignment:CrossAxisAlignment.start ,
+           children: [
+            Row(
+              mainAxisAlignment:MainAxisAlignment.spaceBetween ,
+              children: [
+                Text(
+                  "Foodos",
+                  style: AppWidget.boldTextFieldStyle(),
+                ),
+                Container(
+                  padding: EdgeInsets.all(3),
+                  decoration: BoxDecoration(color:Colors.black,borderRadius:BorderRadius.circular(8)),
+              child:Icon(Icons.shopping_cart_outlined,color: Colors.white,)
+              ,
+            )
+              ],
+            ),
+            SizedBox(height: 20,),
+             Text(
+                  "Delicious Food",
+                  style: AppWidget.HeaderTextFieldStyle(),
+                ),
+                    Text(
+                  "Discover and Get Great Food",
+                  style: AppWidget.HeaderTextFieldStyle(),
+                ),
+            
+          ],
+      
+      ),),
     );
   }
 }
---
+++ lib/Widgets/widget_support.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class AppWidget{
+  static TextStyle boldTextFieldStyle(){
+   return  TextStyle(
+          color: Colors.black,fontSize: 20.0,
+          fontWeight:FontWeight.bold,
+          fontFamily: "Poppins"
+        );
+  }
+    static TextStyle HeaderTextFieldStyle(){
+   return  TextStyle(
+          color: Colors.black,
+          fontSize: 24.0,
+          fontWeight:FontWeight.bold,
+          fontFamily: "Poppins"
+        );
+  }
+    static TextStyle LightTextFieldStyle(){
+   return  TextStyle(
+          color: Colors.black,
+          fontSize: 24.0,
+          fontWeight:FontWeight.bold,
+          fontFamily: "Poppins"
+        );
+  }
+}
