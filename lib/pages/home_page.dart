d616e728e48feef46e467440215220ee377883a5
TAMJID AHMED <ahmed15-4895@diu.edu.bd>
2025-04-12 12:19:30 +0600

    Add first-time user check and redirect to ProfileCreationPage

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -20,3 +20,75 @@ class _HomePageState extends State<HomePage>
         TabController(length: FoodCatagory.values.length, vsync: this);
     _checkFirstTimeUser();
   }
+
+  // Add first-time user check and redirect to ProfileCreationPage
+  Future<void> _checkFirstTimeUser() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? isProfileCreated = prefs.getBool('isProfileCreated');
+
+    if (!isProfileCreated!) {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => ProfileCreationPage()),
+        );
+      });
+    }
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+
+  final List<Widget> _pages = [
+    HomeContent(),
+    CartPage(),
+    ProfilePage(),
+    SettingsPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      backgroundColor: Theme.of(context).colorScheme.surface,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        buttonBackgroundColor: Theme.of(context).colorScheme.surface,
+        color: Theme.of(context).colorScheme.primary,
+        items: [
+          Icon(Icons.home, color: Theme.of(context).colorScheme.inversePrimary),
+          Consumer<Restauarant>(
+            builder: (context, restaurant, child) {
+              final cartItemCount = restaurant.cart.length;
+              return cartItemCount > 0
+                  ? Badge(
+                      label: Text(cartItemCount.toString()),
+                      child: Icon(Icons.shopping_cart,
+                          color: Theme.of(context).colorScheme.inversePrimary),
+                    )
+                  : Icon(Icons.shopping_cart,
+                      color: Theme.of(context).colorScheme.inversePrimary);
+            },
+          ),
+          Icon(Icons.person,
+              color: Theme.of(context).colorScheme.inversePrimary),
+          Icon(Icons.settings,
+              color: Theme.of(context).colorScheme.inversePrimary),
+        ],
+        index: _index,
+        onTap: (index) {
+          setState(() {
+            _index = index;
+          });
+        },
+      ),
+      drawer: MyDrawer(),
+      body: _pages[_index],
+    );
+  }
+}
+
+// ..........
